0f3cd41ead2942ec01f17f639b8bcfa2fa6e4458
s4ijem-san <mejiaseverywhere@gmail.com>
2026-04-03 23:46:50 -0300

    Commit 4 JS - uso de backticks y ${} para variables

--- script.js
+++ script.js
@@ -9,8 +9,8 @@ const email = document.getElementById("email").value;
 
 const mensaje = document.getElementById("mensajePredial");
 
-mensaje.textContent = "Gracias por contratar nuestros servicios " + nombre + ". Hemos recibido la información de tu predio."
-+ "Se enviará un correo a " + email + " con el costo de la revisión y luego de cancelado se enviarán los resultado de la revisión."
+mensaje.textContent = `Gracias por contratar nuestros servicios ${nombre}. Hemos recibido la información de tu predio.
+Se enviará un correo a ${email} con el costo de la revisión y luego de cancelado se enviarán los resultado de la revisión.`
 
 formulario.reset();
 
